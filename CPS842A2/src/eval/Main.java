cc41b70de57264e24a975d60a7e229cf2d6a9b12
Jacky Tran <jackytran98@gmail.com>
2019-10-25 03:12:09 -0400

    removed unused imports

--- CPS842A2/src/eval/Main.java
+++ CPS842A2/src/eval/Main.java
@@ -1,17 +1,8 @@
 //Jacky Tran 500766582
 package eval;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
-import invert.Inverter;
-import invert.Document;
-import invert.FileHandler;
-
 public class Main {
 	public static void main(String[] args) {
-		FileHandler fileHandler = new FileHandler();
-		Inverter inverter = new Inverter();
 		EvalHandler evalHandler = new EvalHandler();
 		evalHandler.test();
 	}
