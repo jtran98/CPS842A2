9dd4d77a139cd4264eaa2cec752be13918165737
Jacky Tran <jackytran98@gmail.com>
2019-10-25 14:40:22 -0400

    changed class to public

--- CPS842A2/src/invert/Stemmer.java
+++ CPS842A2/src/invert/Stemmer.java
@@ -41,7 +41,7 @@ package invert;
   * by calling one of the various stem(something) methods.
   */
 
-class Stemmer
+public class Stemmer
 {  private char[] b;
    private int i,     /* offset into b */
                i_end, /* offset to end of stemmed word */
